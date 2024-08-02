147505676e0b9dc0f416ac473df0bc2ec4df5048
Sebastián Passi <sebastian.passim@uc.cl>
2024-08-02 15:34:44 -0400

    Respaldo 2

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# compound-interest-calculator
+## Introduction
+
+The Compound Interest Calculator is a Python project that allows people to calculate the final amount of money accumulated after a certain period, including regular monthly contributions to an initial deposit.
+
+## Why?
+
+The high inflation present in nowadays affect the value of our money, so it is important that we try to overpass that devaluation and hopefully have profit while doing it. Speaking with a lot of friends I realized that most of them doesn't invest. Actually, a lot of them have their savings "static" in their bank account.
+
+With this program my objetive is to transmit the power of compound interest in people's money and how it is a simple and easy way of increasing patrimony (while avoiding the damage caused by inflation).
+
+## What did I learn?
+
+For the eleboration of this project I applied the basic concepts of frontend and backend. Through this process, I understood the importance of having an organized code. In addition, I focused on proper modularization of the code to achieve high cohesion and low coupling. This approach facilitated maintenance and scalability, allowing easier updates and the addition of new features in the future.
--- frontend/main_window.py
+++ frontend/main_window.py
@@ -20,7 +20,7 @@ class MainWindow(QWidget):
     def initialize_gui(self):
         
         self.position = (200, 200)
-        self.size = (400, 700)
+        self.size = (400, 600)
         self.setGeometry(*self.position, *self.size)
         self.setWindowTitle('Compound interest calculator')
 
@@ -33,6 +33,10 @@ class MainWindow(QWidget):
         pixeles_logo = pixeles_logo.scaled(350, 350)
         self.logo.setPixmap(pixeles_logo)
         vbox.addWidget(self.logo)
+
+        # Problem in data inserted warning
+        self.data_warning = QLabel('', self)
+        vbox.addWidget(self.data_warning)
         
         # Initial deposit
         hbox_1 = QHBoxLayout()
@@ -54,15 +58,81 @@ class MainWindow(QWidget):
 
         # Estimated rate of return
         hbox_3 = QHBoxLayout()
-        self.rate = QLabel('Estimated rate of return: ', self)
+        self.rate = QLabel('Estimated anual rate of return: ', self)
         self.rate.setStyleSheet("font-weight: bold; font-family: Times;")
         self.rate_insert = QLineEdit('', self)
         hbox_3.addWidget(self.rate)
         hbox_3.addWidget(self.rate_insert)
         vbox.addLayout(hbox_3)
 
-        # Calculate botton
-        
+        # Length of time in years
+        hbox_4 = QHBoxLayout()
+        self.length = QLabel('Length of time in years: ')
+        self.length.setStyleSheet("font-weight: bold; font-family: Times;")
+        self.length_insert = QLineEdit('', self)
+        hbox_4.addWidget(self.length)
+        hbox_4.addWidget(self.length_insert)
+        vbox.addLayout(hbox_4)
+
+        # Compound frequency
+        hbox_5 = QHBoxLayout()
+        self.compound_frequency = QLabel('Compound frequency: ', self)
+        self.compound_frequency.setStyleSheet("font-weight: bold; font-family: Times;")
+        self.compound_frequency_insert = QLineEdit('', self)
+        hbox_5.addWidget(self.compound_frequency)
+        hbox_5.addWidget(self.compound_frequency_insert)
+        vbox.addLayout(hbox_5)
+
+        # Calculate & clear all bottons
+        hbox_6 = QHBoxLayout()
+        self.calculate = QPushButton('Calculate', self)
+        self.calculate.setStyleSheet("""
+            QPushButton {
+                background-color: #90EE90;
+                color: black;
+                border: 2px solid #32CD32;
+                border-radius: 10px;
+                font-weight: bold;
+            }
+            QPushButton:hover {
+                background-color: #77DD77;
+            }
+            QPushButton:pressed {
+                background-color: #5EAD5E;
+            }
+        """)
+        self.calculate.clicked.connect(self.calculate_result)
+        self.clear_all_button = QPushButton('Clear', self)
+        self.clear_all_button.setStyleSheet("""
+            QPushButton {
+                background-color: #FF6666;
+                color: black;
+                border: 2px solid #CC0000;
+                border-radius: 10px;
+                font-weight: bold;
+            }
+            QPushButton:hover {
+                background-color: #FF9999;
+            }
+            QPushButton:pressed {
+                background-color: #CC0000;
+            }
+        """)
+             
+        self.clear_all_button.clicked.connect(self.clear_all)
+        hbox_6.addWidget(self.calculate)
+        hbox_6.addWidget(self.clear_all_button)
+        vbox.addLayout(hbox_6)
 
         self.setLayout(vbox)
         self.show()
+
+    def calculate_result(self):
+        pass
+
+    def clear_all(self):
+        self.initial_deposit_insert.clear()
+        self.contributions_insert.clear()
+        self.rate_insert.clear()
+        self.length_insert.clear()
+        self.compound_frequency_insert.clear()
